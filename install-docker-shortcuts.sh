18de5bfb4ae1ed823b3d84a01df16e7214dd8b9c
Jean-Baptiste Claramonte <jeanbaptiste.claramonte@gmail.com>
2016-07-03 17:39:11 +0200

    Update install-docker-shortcuts.sh

--- install-docker-shortcuts.sh
+++ install-docker-shortcuts.sh
@@ -2,3 +2,4 @@ mkdir $HOME/.docker-shortcuts
 curl -o $HOME/.docker-shortcuts/docker-shortcuts.sh https://raw.githubusercontent.com/jbclaramonte/docker-shortcuts/master/docker-shortcuts.sh
 
 echo 'source $HOME/.docker-shortcuts/docker-shortcuts.sh' >> $HOME/.bash_profile
+source $HOME/.bash_profile
