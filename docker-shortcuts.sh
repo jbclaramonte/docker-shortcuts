a2fd0e6e9a8f6e1cc5f48c398aea7d8f7a238a68
Jean-Baptiste Claramonte <jeanbaptiste.claramonte@gmail.com>
2016-07-03 17:53:37 +0200

    Update docker-shortcuts.sh

--- docker-shortcuts.sh
+++ docker-shortcuts.sh
@@ -4,5 +4,5 @@ dversion() {
 
 dupdate() {
   rm $HOME/.docker-shortcuts/docker-shortcuts.sh
-  curl -o $HOME/.docker-shortcuts/docker-shortcuts.sh https://raw.githubusercontent.com/jbclaramonte/docker-shortcuts/master/docker-shortcuts.sh &
+  curl -o $HOME/.docker-shortcuts/docker-shortcuts.sh -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/jbclaramonte/docker-shortcuts/master/docker-shortcuts.sh &
 }
