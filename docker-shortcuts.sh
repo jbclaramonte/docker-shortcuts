14c34d9df0485c67f14d1f16a49fe2b80cb6bf04
Jean-Baptiste Claramonte <jeanbaptiste.claramonte@gmail.com>
2016-07-03 17:42:28 +0200

    Update docker-shortcuts.sh

--- docker-shortcuts.sh
+++ docker-shortcuts.sh
@@ -3,6 +3,7 @@ dversion() {
 }
 
 dupdate() {
+  rm $HOME/.docker-shortcuts/docker-shortcuts.sh
   curl -o $HOME/.docker-shortcuts/docker-shortcuts.sh https://raw.githubusercontent.com/jbclaramonte/docker-shortcuts/master/docker-shortcuts.sh
   source $HOME/.bash_profile
 }
